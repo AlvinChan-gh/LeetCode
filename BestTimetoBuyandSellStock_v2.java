0507faee803e848300061b57797a556e543e14a3
BuildTools <xalvinchanx@hotmail.ca>
2021-08-17 16:16:42 -0400

    Update BestTimetoBuyandSellStock_v2.java

--- BestTimetoBuyandSellStock_v2.java
+++ BestTimetoBuyandSellStock_v2.java
@@ -2,13 +2,17 @@ class Solution {
     public int maxProfit(int[] prices) {
         int maxProfit = 0;
         int profit = 0;
-        for(int s = 0; s < prices.length; s++){
-            for(int l = s; l < prices.length; l++){
-                profit = prices[l] -prices[s];
-                if (profit > maxProfit){
-                    maxProfit = profit;
-                }
+        int i = 1;
+        int low = prices[0];
+        while (i < prices.length){
+            profit = prices[i] - low;
+            if (profit < 0){
+                low = prices[i];
             }
+            if (profit > maxProfit){
+                maxProfit = profit;
+            }
+            i++;
         }
         return maxProfit;
     }
